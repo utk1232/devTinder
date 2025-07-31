ac5e37bd148f0838d0ce8fb3381a99c8ec119771
utk1232 <138392991+utk1232@users.noreply.github.com>
2025-07-31 21:51:36 +0530

    database connection and signup route

--- src/app.js
+++ src/app.js
@@ -3,20 +3,75 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "John Doe",
-        lastName: "Doe",
-        emailId: "john.doe@example.com",
-        password: "password123",
-    });
-
-
-    await user.save();
-    res.send("User registered successfully");
-
+    console.log(req.body);
+    //creating a new instance of User model
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.send("User registered successfully");
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(400).send("Internal Server Error");
+    }
 });
+
+
+// get all users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await User.find({});
+        res.send(user);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(400).send("Internal Server Error");
+    }
+});
+
+// get a user by emailId
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        console.log("Fetching user with email:", userEmail);
+        const user = await User.findOne({ emailId: userEmail });
+        res.send(user);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(400).send("Internal Server Error");
+    }
+}); 
+
+//delete a user by id
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        console.log("Deleting user with ID:", userId);
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(400).send("Internal Server Error");
+    }
+});
+
+//update data of a user
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    console.log("Update Data:", data);
+    try {
+        console.log("Updating user with ID:", userId);
+        const user = await User.findByIdAndUpdate({ _id: userId }, data);
+        res.send("user updated successfully");
+    } catch (error) {
+        console.error("Error updating user:", error);
+        res.status(400).send("Internal Server Error");
+    }
+});
+
+
 
 connectDB().then(() => {
     console.log('Connected to MongoDB');
